aa4de82be9d83487fc6944a4d3ce3508027bd209
Hans <sivhans3@gmail.com>
2026-07-13 19:06:09 +0200

    Unit 1: Completing additional resources - 51% done

---
+++ Pre-Academy/Unit 1/Lesson 3.py
@@ -0,0 +1,9 @@
+# Strings
+course = 'Python for beginners'
+
+# Strings are immutable
+print(course.upper())
+print(course.find('o')) # Returns index of the first occurence of the char(s)
+print(course.replace('for', '4')) # Replaces chars in a string with other chars
+
+print('Python' in course) # Returns boolean stating whether a sequence exists in a variable or sequence
---
+++ Pre-Academy/Unit 1/Lesson 4.py
@@ -0,0 +1,14 @@
+# Operators 
+
+# Arithmetic Operators(+, -, /, *, **(exponent), //(int division), %(modulus))
+
+x = 10
+x += 3 # Augmented Assignment Operator (+=, -=, *=, /=)
+
+# Comparison operators (<, >, <=, >=, ==, !=)
+
+print(5 >= 7)
+
+# Logical Operators (and, or)
+price = 5
+print(price > 10 or price < 30)
