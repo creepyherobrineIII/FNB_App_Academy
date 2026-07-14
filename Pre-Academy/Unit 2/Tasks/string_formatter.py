d9f9acbb43583b1e6d7fd5958d3835d3933144bf
Hans <sivhans3@gmail.com>
2026-07-14 15:35:08 +0200

    Unit 2: Completed Practical Task

---
+++ Pre-Academy/Unit 2/Tasks/string_formatter.py
@@ -0,0 +1,15 @@
+# Data Collection 
+first_name = input("Enter your first name: ").strip()
+last_name = input("Enter your last name: ").strip()
+bio = input("Enter a short description of yourself: ").strip()
+
+# Manipulating strings
+editedBio = bio.replace("I am", "I'm")
+full_name = first_name + last_name
+# Username creation
+username = first_name[0].lower() + last_name.lower()
+
+# Output
+print(f"Username: {username}")
+print(f"Full Name: {full_name.title()}")
+print(f"Bio: {editedBio}\nNumber of characterss in the bio: {len(editedBio)}")
