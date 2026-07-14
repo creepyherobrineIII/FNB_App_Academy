0d66cb4e0b1d63fbbce04c7b536ea68becdb2aaa
Hans <sivhans3@gmail.com>
2026-07-14 19:30:40 +0200

    Unit 5: Completed video lesson

---
+++ Pre-Academy/Unit 5/Lessons/Lesson 1.py
@@ -0,0 +1,28 @@
+# A countdown using a while loop
+''' 
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("Blast Off!!")
+
+# Building a simple rep counter
+
+for rep in range(1, 4):
+    print(f"This is rep no.{rep}")
+'''
+
+# A guessing game
+
+secret_word = "python"
+
+while True:
+    guess = input("Guess the programming language we are using: ").lower()
+
+    if guess == secret_word:
+        print("You guessed the correct language!")
+        break
+    else:
+        print("Incorrect guess. Try again")
